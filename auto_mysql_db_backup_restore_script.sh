0cfad4d81f11dedebe1984a3d47cef6cd7a20b1c
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2022-12-12 10:41:29 +0530

    updated

--- auto_mysql_db_backup_restore_script.sh
+++ auto_mysql_db_backup_restore_script.sh
@@ -67,3 +67,78 @@ echo "Successfully created DB backup at $backup_date."
 # ssh -i /home/ashfaque/.ssh/centosvm_ed25519 v3@192.168.0.100 -p 2222 "mysqldump -uremoteuser -p123456 -P33060 dump_db" > dump_db.sql
 
 
+# Generation of ssh key pairs:
+# sudo apt-get install openssh-server
+# sudo systemctl enable ssh
+# sudo systemctl start ssh
+# ssh v3@192.168.0.100 -p 22
+
+# In host PC:
+# ~$ ssh-keygen -t rsa    or,    ssh-keygen -t ed25519
+# ~$ ssh-copy-id -i ~/.ssh/id_rsa.pub v3@192.168.0.100/home/v3/.ssh/authorized_keys
+# or,
+# ~$ type $env:USERPROFILE\.ssh\id_rsa.pub | ssh v3@192.168.0.100 -p 2200 "cat >> .ssh/authorized_keys"
+# or, echo ssh-ed25519 lksdjfHYJKKJKJHJKkj7856JHKHJKee0ojufUrfq+wA5lGsbT+tD/ Ashfaque@ASH > authorized_keys   # in Server
+# ~$ ssh -i ~/.ssh/id_rsa v3@192.168.0.100 -p 2200
+# Edit ~/.ssh/config file:
+#     Host cppvm
+#     HostName 192.168.0.100
+#     User v3
+#     Port 2200
+#     IdentityFile ~/.ssh/private_key
+
+# ~$ ssh v3@cppvm
+
+# in ~/.bashrc alias centos='ssh v3@cppvm'
+
+
+# To convert private key to pem file. Open puttygen, goto Conversion tab and import key, choose RSA, DSA, ECDSA, EdDSA, SSH-1(RSA) and then you can either save `public or private key` by clicking on that button. Or you can go to : Conversions tab and click on `Export OpenSSH key (Force New Format)` and save it as .pem file.
+
+# To extract compatible public key from a private key use: `ssh-keygen -f private.pem -y > private.pub`
+
+
+
+# MariaDB installation:
+# sudo apt update
+
+# sudo apt install mariadb-server
+
+# systemctl status mariadb
+
+# sudo mysql_secure_installation 
+
+# mysql -u root -p
+
+# SELECT VERSION();
+
+# CREATE USER remoteuser@localhost IDENTIFIED BY '123456';
+# CREATE DATABASE dump_db;
+# GRANT ALL ON dump_db.* TO remoteuser@'%' IDENTIFIED BY '123456' WITH GRANT OPTION;
+
+# CREATE USER admin@localhost IDENTIFIED BY 'admin';
+# GRANT ALL ON *.* TO admin@'localhost' IDENTIFIED BY 'admin' WITH GRANT OPTION;
+
+
+# FLUSH PRIVILEGES;
+
+# mysql -u test_user -p
+
+# SHOW DATABASES;
+
+# nano /etc/mysql/my.cnf
+
+# 	[mysqld]
+# 	bind-address = 0.0.0.0
+
+
+# systemctl restart mariadb
+
+# netstat -ant | grep 3306
+# ps -ef | grep -i mysql
+
+
+# vi /etc/my.cnf.d/mariadb-server.cnf -> uncomment bind-address = 0.0.0.0    # CentOS 8
+# firewall-cmd --add-port=3306/tcp
+# firewall-cmd --permanent --add-port=3306/tcp
+# systemctl restart mariadb
+
